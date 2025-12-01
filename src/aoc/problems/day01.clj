5346fbf5d8306ed9cab1b0d97b241a73153fb8c7
Jaxon Adams <jaxon.adams@loanpro.io>
2025-12-01 08:27:58 -0700

    day01: Count resulting zeroes when executing instructions

--- src/aoc/problems/day01.clj
+++ src/aoc/problems/day01.clj
@@ -10,16 +10,23 @@
         num-clicks (->> instruction rest (string/join #"") Integer/parseInt)]
     [op num-clicks]))
 
-(defn- turn-dial [current-pos instruction]
-  (let [[turn-fn num-clicks] (parse-instruction instruction)]
-    (-> current-pos
-        (turn-fn num-clicks)
-        (mod 100))))
+(defn- turn-dial [pos-info instruction]
+  (let [[current-pos num-zeros] pos-info
+        [turn-fn num-clicks] (parse-instruction instruction)
+        new-pos (-> current-pos
+                    (turn-fn num-clicks)
+                    (mod 100))]
+    [new-pos (if (zero? new-pos) (inc num-zeros) num-zeros)]))
 
-(defn- get-password [rotations]
-  (reduce turn-dial 50 rotations))
+(defn- execute-instructions [rotations]
+  (reduce turn-dial [50 0] rotations))
 
-;; AOC example:
+(defn get-password [rotations]
+  (-> rotations
+      execute-instructions
+      second))
+
+;; AOC example (should equal 3):
 (comment
   (get-password ["L68"
                  "L30"
@@ -31,4 +38,9 @@
                  "L99"
                  "R14"
                  "L82"]))
+
+(comment
+  (-> "input/day01/problem-1-input.txt"
+      read-file-lines
+      get-password))
 
