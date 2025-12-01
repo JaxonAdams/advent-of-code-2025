43e63395f0dc88eab533cabdd00e819313c44118
Jaxon Adams <jaxon.adams@loanpro.io>
2025-12-01 09:06:36 -0700

    day01: Adjust solution to count all times the dial passes zero

--- src/aoc/problems/day01.clj
+++ src/aoc/problems/day01.clj
@@ -2,6 +2,21 @@
   (:require [clojure.string :as string]
             [utils.files :refer [read-file-lines]]))
 
+(defn- floor-of-div-100 [n]
+  (-> n
+      (/ 100.0)
+      Math/floor
+      int))
+
+(defn- count-zero-crossings [current-pos turn-fn num-clicks]
+  (let [raw-new-pos ((if (= turn-fn +) + -) current-pos num-clicks)]
+    (if (= turn-fn +)
+      (let [p-start-adjusted (if (zero? current-pos) 100 current-pos)]
+        (- (floor-of-div-100 raw-new-pos)
+           (floor-of-div-100 (dec p-start-adjusted))))
+      (- (floor-of-div-100 (dec current-pos))
+         (floor-of-div-100 (dec raw-new-pos))))))
+
 (defn- parse-instruction [instruction]
   (let [op (cond
              (-> instruction first (= \R)) +
@@ -13,10 +28,14 @@
 (defn- turn-dial [pos-info instruction]
   (let [[current-pos num-zeros] pos-info
         [turn-fn num-clicks] (parse-instruction instruction)
-        new-pos (-> current-pos
-                    (turn-fn num-clicks)
-                    (mod 100))]
-    [new-pos (if (zero? new-pos) (inc num-zeros) num-zeros)]))
+        raw-new-pos (turn-fn current-pos num-clicks)
+
+        new-pos (mod (mod raw-new-pos 100) 100)
+
+        times-passed-zero (count-zero-crossings current-pos turn-fn num-clicks)]
+
+    [new-pos
+     (+ num-zeros times-passed-zero)]))
 
 (defn- execute-instructions [rotations]
   (reduce turn-dial [50 0] rotations))
@@ -26,7 +45,7 @@
       execute-instructions
       second))
 
-;; AOC example (should equal 3):
+;; AOC example (should equal 6):
 (comment
   (get-password ["L68"
                  "L30"
@@ -38,6 +57,9 @@
                  "L99"
                  "R14"
                  "L82"]))
+
+(comment
+  (get-password ["R1000"]))
 
 (comment
   (-> "input/day01/problem-1-input.txt"
