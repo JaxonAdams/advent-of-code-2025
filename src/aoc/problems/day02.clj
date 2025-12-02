43e63a6dc0a2327724f561af2f9c8ba9b909b747
Jaxon Adams <jaxon.adams@loanpro.io>
2025-12-01 22:19:45 -0700

    day02: Check ranges for invalid IDs

--- src/aoc/problems/day02.clj
+++ src/aoc/problems/day02.clj
@@ -1,1 +1,27 @@
-(ns aoc.problems.day02)
+(ns aoc.problems.day02
+  (:require [clojure.string :as string]))
+
+(defn- parse-range [range-str]
+  (let [range-str-split (string/split range-str #"-" 2)
+        [start stop] (map Integer/parseInt range-str-split)]
+    (range start (inc stop))))
+
+(defn- is-invalid-id? [num]
+  (if (-> num str count (mod 2) zero? not)
+    false
+    (let [n-str (str num)
+          first-half (apply str (take (/ (count n-str) 2) n-str))
+          second-half (apply str (take-last (/ (count n-str) 2) n-str))]
+      (= first-half second-half))))
+
+(defn- get-invalid-ids
+  "Get all 'invalid' IDs in a given range [START, STOP] (formated START-STOP).
+   An ID is invalid if it consists solely of some sequence of digits repeated twice;
+   e.g. 55 (five twice), 6464 (64 twice), and 123123 (123 twice)."
+  [range-str]
+  (let [range-to-check (parse-range range-str)]
+    (filter is-invalid-id? range-to-check)))
+
+;; Examples from AOC
+(comment
+  (get-invalid-ids "11-22"))
