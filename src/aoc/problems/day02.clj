441753f93aca8a8a2f56b84a6b005a2082ee7437
Jaxon Adams <jaxon.adams@loanpro.io>
2025-12-01 22:40:26 -0700

    day02: Implement function for checking a list of ranges

--- src/aoc/problems/day02.clj
+++ src/aoc/problems/day02.clj
@@ -3,7 +3,7 @@
 
 (defn- parse-range [range-str]
   (let [range-str-split (string/split range-str #"-" 2)
-        [start stop] (map Integer/parseInt range-str-split)]
+        [start stop] (map Long/parseLong range-str-split)]
     (range start (inc stop))))
 
 (defn- is-invalid-id? [num]
@@ -22,6 +22,40 @@
   (let [range-to-check (parse-range range-str)]
     (filter is-invalid-id? range-to-check)))
 
+(defn get-invalid-id-sum
+  [range-strs]
+  (->> range-strs
+       (map get-invalid-ids)
+       flatten
+       (reduce +)))
+
 ;; Examples from AOC
 (comment
-  (get-invalid-ids "11-22"))
+  ;; 11 and 22
+  (get-invalid-ids "11-22")
+  ;; 99
+  (get-invalid-ids "95-115")
+  ;; 1010
+  (get-invalid-ids "998-1012")
+  ;; 1188511885
+  (get-invalid-ids "1188511880-1188511890"))
+
+;; Solution example from AOC
+(comment
+  ;; 1227775554
+  (get-invalid-id-sum ["11-22"
+                       "95-115"
+                       "998-1012"
+                       "1188511880-1188511890"
+                       "222220-222224"
+                       "1698522-1698528"
+                       "446443-446449"
+                       "38592856-38593862"]))
+
+;; Check using the actual input...
+(comment
+  (let [ranges (-> "input/day02/day02-input.txt"
+                   slurp
+                   (string/split #",")
+                   (->> (map string/trim)))]
+    (get-invalid-id-sum ranges)))
