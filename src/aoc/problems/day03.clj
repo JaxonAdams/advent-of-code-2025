11addd8fe32bcc6af3d72e5599bfd9ae24278777
Jaxon Adams <jaxon.adams@loanpro.io>
2025-12-02 22:26:07 -0700

    day03: Add the ability to find the max voltage from a bank

--- src/aoc/problems/day03.clj
+++ src/aoc/problems/day03.clj
@@ -1,1 +1,28 @@
-(ns aoc.problems.day03)
+(ns aoc.problems.day03
+  (:require [clojure.string :as string]))
+
+(defn- max-voltage [battery-bank]
+  (let [voltages (-> battery-bank
+                     (string/split #"")
+                     (->> (map Integer/parseInt)
+                          (map-indexed vector)))
+        tens-candidates (take (dec (count battery-bank)) voltages)
+        ones-candidates voltages
+        sorted-tens-by-voltage (reverse (sort-by second tens-candidates))
+        sorted-ones-by-voltage (reverse (sort-by second ones-candidates))
+        max-voltage (first sorted-tens-by-voltage)
+        next-max-voltage (first (filter #(-> % first (> (first max-voltage))) sorted-ones-by-voltage))]
+    (->> [max-voltage next-max-voltage]
+         (map second)
+         (apply str)
+         Integer/parseInt)))
+
+(comment
+  ;; 98
+  (max-voltage "987654321111111")
+  ;; 89
+  (max-voltage "811111111111119")
+  ;; 78
+  (max-voltage "234234234234278")
+  ;; 92
+  (max-voltage "818181911112111"))
