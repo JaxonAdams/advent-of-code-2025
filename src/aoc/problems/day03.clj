0c144fdbc4a1c0b2e3183393b22c899e933cc6af
Jaxon Adams <jaxon.adams@loanpro.io>
2025-12-02 22:44:17 -0700

    day03: Get the solution to part 1

--- src/aoc/problems/day03.clj
+++ src/aoc/problems/day03.clj
@@ -1,21 +1,26 @@
 (ns aoc.problems.day03
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string]
+            [utils.files :refer [read-file-lines]]))
 
 (defn- max-voltage [battery-bank]
   (let [voltages (-> battery-bank
                      (string/split #"")
                      (->> (map Integer/parseInt)
                           (map-indexed vector)))
-        tens-candidates (take (dec (count battery-bank)) voltages)
-        ones-candidates voltages
-        sorted-tens-by-voltage (reverse (sort-by second tens-candidates))
-        sorted-ones-by-voltage (reverse (sort-by second ones-candidates))
-        max-voltage (first sorted-tens-by-voltage)
-        next-max-voltage (first (filter #(-> % first (> (first max-voltage))) sorted-ones-by-voltage))]
+        tens-candidates (reverse (sort-by second (take (dec (count battery-bank)) voltages)))
+        max-voltages-tens (sort-by first (filter #(-> tens-candidates first second (= (second %))) tens-candidates))
+        max-voltage (first max-voltages-tens)
+        ones-candidates (reverse (sort-by second (second (split-at (-> max-voltage first inc) voltages))))
+        next-max-voltage (first ones-candidates)]
     (->> [max-voltage next-max-voltage]
          (map second)
          (apply str)
          Integer/parseInt)))
+
+(defn sum-of-max-volts [battery-banks]
+  (->> battery-banks
+       (map max-voltage)
+       (reduce +)))
 
 (comment
   ;; 98
@@ -26,3 +31,22 @@
   (max-voltage "234234234234278")
   ;; 92
   (max-voltage "818181911112111"))
+
+;; EDGE CASES
+(comment
+  ;; 99
+  (max-voltage "997654321111111"))
+
+(comment
+  ;; 357
+  (sum-of-max-volts ["987654321111111"
+                     "811111111111119"
+                     "234234234234278"
+                     "818181911112111"]))
+
+;; Part 1 Solution...
+(comment
+  ;; NOT 17323
+  (->  "input/day03/input.txt"
+       read-file-lines
+       sum-of-max-volts))
