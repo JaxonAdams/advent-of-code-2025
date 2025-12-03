1b2db2b423fac303c645fa0ab1175bf6a3b29393
Jaxon Adams <jaxon.adams@loanpro.io>
2025-12-02 23:07:22 -0700

    day03: Find the solution using twelve batteries instead of two

--- src/aoc/problems/day03.clj
+++ src/aoc/problems/day03.clj
@@ -2,6 +2,29 @@
   (:require [clojure.string :as string]
             [utils.files :refer [read-file-lines]]))
 
+;; TODO: Come back and clean this up!
+;; I got the right answer but this code could use some work...
+
+(defn- parse-digits-with-idx
+  [s]
+  (->> (string/split s #"")
+       (map #(Integer/parseInt %))
+       (map-indexed vector)
+       vec))
+
+(defn largest-subseq-from
+  [v start k]
+  (let [n (count v)]
+    (loop [i start, remaining k, chosen []]
+      (if (zero? remaining)
+        chosen
+        (let [last-idx (- n remaining)
+              candidates (subvec v i (inc last-idx))
+              max-digit (apply max (map second candidates))
+              pick     (first (filter #(= max-digit (second %)) candidates))
+              p-idx    (first pick)]
+          (recur (inc p-idx) (dec remaining) (conj chosen pick)))))))
+
 (defn- max-voltage [battery-bank]
   (let [voltages (-> battery-bank
                      (string/split #"")
@@ -17,10 +40,31 @@
          (apply str)
          Integer/parseInt)))
 
+(defn- max-voltage-twelve-batteries [battery-bank]
+  (let [v (parse-digits-with-idx battery-bank)
+        n (count v)
+        tens-slice (subvec v 0 (- n 11))
+        max-digit (apply max (map second tens-slice))
+        first-pick (first (filter #(= (second %) max-digit) tens-slice))
+        first-idx  (first first-pick)
+        rest-picks (largest-subseq-from v (inc first-idx) 11)]
+    (->> (cons first-pick rest-picks)
+         (map second)
+         (apply str)
+         Long/parseLong)))
+
 (defn sum-of-max-volts [battery-banks]
   (->> battery-banks
        (map max-voltage)
        (reduce +)))
+
+(defn sum-of-max-volts-twelve-batteries [battery-banks]
+  (->> battery-banks
+       (map max-voltage-twelve-batteries)
+       (reduce +)))
+
+;; ----------------------------------------------------------------------------
+;; PART 1
 
 (comment
   ;; 98
@@ -46,7 +90,37 @@
 
 ;; Part 1 Solution...
 (comment
-  ;; NOT 17323
-  (->  "input/day03/input.txt"
-       read-file-lines
-       sum-of-max-volts))
+  (-> "input/day03/input.txt"
+      read-file-lines
+      sum-of-max-volts))
+
+;; ----------------------------------------------------------------------------
+;; PART 2
+
+(comment
+  ;; 987654321111
+  (max-voltage-twelve-batteries "987654321111111")
+  ;; 811111111119
+  (max-voltage-twelve-batteries "811111111111119")
+  ;; 434234234278
+  (max-voltage-twelve-batteries "234234234234278")
+  ;; 888911112111
+  (max-voltage-twelve-batteries "818181911112111"))
+
+;; EDGE CASES
+(comment
+  ;; 999999876549
+  (max-voltage-twelve-batteries "999999876543219"))
+
+(comment
+  ;; 3121910778619
+  (sum-of-max-volts-twelve-batteries ["987654321111111"
+                                      "811111111111119"
+                                      "234234234234278"
+                                      "818181911112111"]))
+
+;; Part 2 solution...
+(comment
+  (-> "input/day03/input.txt"
+      read-file-lines
+      sum-of-max-volts-twelve-batteries))
