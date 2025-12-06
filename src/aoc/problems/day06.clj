a1f1473deaa6c2bb384f67c79873d3e24ff4c3d8
Jaxon Adams <jaxon.adams@loanpro.io>
2025-12-05 22:37:05 -0700

    day06: Group operands and operators to make solving easier

--- src/aoc/problems/day06.clj
+++ src/aoc/problems/day06.clj
@@ -1,1 +1,27 @@
-(ns aoc.problems.day06)
+(ns aoc.problems.day06
+  (:require [clojure.string :as string]))
+
+(defn- parse-homework [homework-lines]
+  (let [split-on-spaces (->> homework-lines
+                             (map string/trim)
+                             (map #(string/split % #"\s+")))
+        operands (->> split-on-spaces
+                      butlast
+                      (map #(map Long/parseLong %)))
+        operations (last split-on-spaces)]
+    (map-indexed
+     (fn [idx operation]
+       (into [operation]
+             (map #(nth % idx) operands)))
+     operations)))
+
+;; ----------------------------------------------------------------------------
+;; PART ONE
+
+(comment
+  (def problems ["123 328  51 64 "
+                 " 45 64  387 23 "
+                 "  6 98  215 314"
+                 "*   +   *   +  "])
+
+  (parse-homework problems))
