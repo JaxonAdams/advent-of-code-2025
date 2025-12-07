f6c3e1ad5de1b03410cbf8fc0e96e1816112438c
Jaxon Adams <jaxon.adams@loanpro.io>
2025-12-06 22:57:27 -0700

    day07: Fill out tachyon diagram and count splits

--- src/aoc/problems/day07.clj
+++ src/aoc/problems/day07.clj
@@ -1,1 +1,72 @@
-(ns aoc.problems.day07)
+(ns aoc.problems.day07
+  (:require [clojure.string :as string]))
+
+(defn- diagram-rows->matrix [diagram-rows]
+  (mapv #(string/split % #"") diagram-rows))
+
+(defn- get-next-cell [row-idx col-idx diagram-matrix]
+  (let [num-cols (-> diagram-matrix first count)]
+    (if (>= col-idx (dec num-cols))
+      [(inc row-idx) 0]
+      [row-idx (inc col-idx)])))
+
+(defn- run-tachyon-simulation [diagram-matrix]
+  (loop [row-idx 1
+         col-idx 0
+         num-splits 0
+         updated-matrix diagram-matrix]
+    (if (>= row-idx (count diagram-matrix))
+      {:matrix updated-matrix
+       :num-splits num-splits}
+      (let [cell (get-in updated-matrix [row-idx col-idx])
+            cell-above (get-in updated-matrix [(dec row-idx) col-idx])
+            [next-row-idx next-col-idx] (get-next-cell row-idx col-idx updated-matrix)]
+        (cond
+          (and
+           (= cell-above "|")
+           (= cell "^")) (recur
+                          next-row-idx
+                          next-col-idx
+                          (inc num-splits)
+                          (-> updated-matrix
+                              (assoc-in [row-idx (dec col-idx)] "|")
+                              (assoc-in [row-idx (inc col-idx)] "|")))
+          (or
+           (= cell-above "S")
+           (= cell-above "|")) (recur
+                                next-row-idx
+                                next-col-idx
+                                num-splits
+                                (assoc-in updated-matrix [row-idx col-idx] "|"))
+          :else (recur
+                 next-row-idx
+                 next-col-idx
+                 num-splits
+                 updated-matrix))))))
+
+;; ----------------------------------------------------------------------------
+;; PART ONE
+
+(comment
+  (def example-diagram [".......S......."
+                        "..............."
+                        ".......^......."
+                        "..............."
+                        "......^.^......"
+                        "..............."
+                        ".....^.^.^....."
+                        "..............."
+                        "....^.^...^...."
+                        "..............."
+                        "...^.^...^.^..."
+                        "..............."
+                        "..^...^.....^.."
+                        "..............."
+                        ".^.^.^.^.^...^."
+                        "..............."])
+
+  (diagram-rows->matrix example-diagram)
+
+  (-> example-diagram
+      diagram-rows->matrix
+      run-tachyon-simulation))
