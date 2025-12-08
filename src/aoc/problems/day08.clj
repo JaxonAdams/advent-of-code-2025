b9a26a4f6b2d5e3da52b91c041fc815c9902b5f0
Jaxon Adams <jaxon.adams@loanpro.io>
2025-12-08 09:18:04 -0700

    day08: Implement Kruskal's & Union-Find to get junction box MST

--- src/aoc/problems/day08.clj
+++ src/aoc/problems/day08.clj
@@ -51,6 +51,81 @@
        (map count)
        (reduce *)))
 
+(defn- all-edges
+  [points]
+  (let [pts (vec points)
+        n (count pts)]
+    (into []
+          (for [i (range n)
+                j (range (inc i) n)]
+            {:u i :v j :w (straight-line-distance (pts i) (pts j))}))))
+
+;; Union-Find dataset for usage in Kruskal's algorithm
+(defn- uf-find
+  [parents x]
+  (letfn [(find-path [p i stack]
+            (let [parent (p i)]
+              (if (= parent i)
+                [i stack]
+                (recur p parent (conj stack i)))))]
+    (let [[root path] (find-path parents x [])]
+      (if (seq path)
+        [root (reduce (fn [acc idx] (assoc acc idx root)) parents path)]
+        [root parents]))))
+
+(defn- uf-union
+  [parents ranks x y]
+  (let [[rx parents'] (uf-find parents x)
+        [ry parents''] (uf-find parents' y)]
+    (if (= rx ry)
+      [parents'' ranks false]
+      (let [rrx (ranks rx)
+            rry (ranks ry)]
+        (cond
+          (< rrx rry) [(assoc parents'' rx ry) ranks true]
+          (> rrx rry) [(assoc parents'' ry rx) ranks true]
+          :else [(assoc parents'' ry rx) (assoc ranks rx (inc rrx)) true])))))
+
+;; Kruskal algorithm for finding MST
+(defn- kruskal-mst
+  [points]
+  (let [pts (vec points)
+        n (count pts)]
+    (if (<= n 1)
+      {:mst-edges [] :total-weight 0.0}
+      (let [edges (->> (all-edges pts) (sort-by :w))
+            parents (vec (range n))
+            ranks (vec (repeat n 0))]
+        (loop [remaining edges
+               parents parents
+               ranks ranks
+               mst []
+               total 0.0]
+          (cond
+            (or (empty? remaining) (= (count mst) (dec n)))
+            {:mst-edges mst :total-weight total}
+
+            :else
+            (let [{:keys [u v w] :as e} (first remaining)
+                  [parents' ranks' did-union] (uf-union parents ranks u v)]
+              (recur (rest remaining)
+                     parents'
+                     ranks'
+                     (if did-union
+                       (conj mst (assoc e :pu (nth pts u) :pv (nth pts v)))
+                       mst)
+                     (if did-union (+ total w) total)))))))))
+
+(defn- final-boxes-x-product [junction-boxes]
+  (-> junction-boxes
+      kruskal-mst
+      :mst-edges
+      last
+      (select-keys [:pu :pv])
+      vals
+      (->> (map first)
+           (reduce *))))
+
 ;; ----------------------------------------------------------------------------
 ;; PART ONE
 
@@ -89,3 +164,37 @@
       (->> (mapv #(string/split % #","))
            (mapv #(mapv Long/parseLong %)))
       (largest-circuits-product 1000)))
+
+;; ----------------------------------------------------------------------------
+;; PART TWO
+
+(comment
+
+  (def example-distances [[162 817 812]
+                          [57 618 57]
+                          [906 360 560]
+                          [592 479 940]
+                          [352 342 300]
+                          [466 668 158]
+                          [542 29 236]
+                          [431 825 988]
+                          [739 650 466]
+                          [52 470 668]
+                          [216 146 977]
+                          [819 987 18]
+                          [117 168 530]
+                          [805 96 715]
+                          [346 949 466]
+                          [970 615 88]
+                          [941 993 340]
+                          [862 61 35]
+                          [984 92 344]
+                          [425 690 689]])
+
+  (def test-pts [[0 0 0] [0 0 1] [0 1 0] [1 0 0]])
+  (kruskal-mst test-pts)
+
+  (kruskal-mst example-distances)
+
+  ;; 25272
+  (final-boxes-x-product example-distances))
