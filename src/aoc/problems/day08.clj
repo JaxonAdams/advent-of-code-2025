5d3884494815932abd7ecdfbd16179adcfe5f862
Jaxon Adams <jaxon.adams@loanpro.io>
2025-12-07 22:55:48 -0700

    day08: Sort junction boxes by distance in relation to each other

--- src/aoc/problems/day08.clj
+++ src/aoc/problems/day08.clj
@@ -1,1 +1,44 @@
 (ns aoc.problems.day08)
+
+(defn- straight-line-distance
+  "https://en.wikipedia.org/wiki/Euclidean_distance"
+  [p1 p2]
+  (Math/sqrt
+   (reduce + (map #(Math/pow (- %2 %1) 2) p1 p2))))
+
+(defn- get-all-distances [junction-boxes]
+  (->> (for [a junction-boxes
+             b junction-boxes
+             :when (not= a b)]
+         [(sort [a b]) (straight-line-distance a b)])
+       (sort-by second)
+       dedupe))
+
+;; ----------------------------------------------------------------------------
+;; PART ONE
+
+(comment
+
+  (def example-distances [[162 817 812]
+                          [57 618 57]
+                          [906 360 560]
+                          [592 479 940]
+                          [352 342 300]
+                          [466 668 158]
+                          [542 29 236]
+                          [431 825 988]
+                          [739 650 466]
+                          [52 470 668]
+                          [216 146 977]
+                          [819 987 18]
+                          [117 168 530]
+                          [805 96 715]
+                          [346 949 466]
+                          [970 615 88]
+                          [941 993 340]
+                          [862 61 35]
+                          [984 92 344]
+                          [425 690 689]])
+
+  (->> example-distances
+       get-all-distances))
