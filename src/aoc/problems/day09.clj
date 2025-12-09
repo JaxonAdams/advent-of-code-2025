617e1085bca4f1514e48cd2d00e6c017884b5e74
Jaxon Adams <jaxon.adams@loanpro.io>
2025-12-08 22:50:18 -0700

    day09: Use ray-casting algo to check if rect is contained in polygon

--- src/aoc/problems/day09.clj
+++ src/aoc/problems/day09.clj
@@ -11,12 +11,31 @@
         :when (not= a b)]
     {:corners [a b] :area (rect-area a b)}))
 
-(defn- largest-rect-red-corners [red-tile-locations]
+(defn largest-rect-red-corners [red-tile-locations]
   (->> red-tile-locations
        possible-rects-red-corners
        (sort-by :area >)
        first
        :area))
+
+(defn- midpoint [[r1 c1] [r2 c2]]
+  [(/ (+ r1 r2) 2.0)
+   (/ (+ c1 c2) 2.0)])
+
+(defn rect-in-polygon? [[p1 p2] polygon]
+  (let [[px py] (midpoint p1 p2)
+        n (count polygon)]
+    (loop [i 0
+           j (dec n)
+           inside false]
+      (if (= i n)
+        inside
+        (let [[x1 y1] (nth polygon i)
+              [x2 y2] (nth polygon j)
+              intersect (and (not= y1 y2)
+                             (<= (min y1 y2) py (max y1 y2))
+                             (> px (+ x1 (* (- x2 x1) (/ (- py y1) (- y2 y1))))))]
+          (recur (inc i) i (if intersect (not inside) inside)))))))
 
 ;; ----------------------------------------------------------------------------
 ;; PART ONE
@@ -48,3 +67,23 @@
        (map #(split % #","))
        (map #(map Long/parseLong %))
        largest-rect-red-corners))
+
+;; ----------------------------------------------------------------------------
+;; PART TWO
+
+(comment
+
+  (nth example-red-tile-locations 1)
+
+  (def simple-square [[0 0]
+                      [10 0]
+                      [10 10]
+                      [0 10]])
+
+  (rect-in-polygon? [[0 0] [1 10]] simple-square)
+
+  (-> example-red-tile-locations
+      possible-rects-red-corners
+      first
+      :corners
+      (rect-in-polygon? example-red-tile-locations)))
