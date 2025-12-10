43536d2f83ddaad50a167b983ac433b5fcb0e041
Jaxon Adams <jaxon.adams@loanpro.io>
2025-12-09 23:47:23 -0700

    day10: Check min number of button presses to configure joltage

--- src/aoc/problems/day10.clj
+++ src/aoc/problems/day10.clj
@@ -3,6 +3,8 @@
             [clojure.math.combinatorics :as combo]
             [utils.files :refer [read-file-lines]]))
 
+;; TODO: clean up this code!
+
 (defn- parse-joltage-diagram [diagram-str]
   (-> diagram-str
       rest
@@ -35,26 +37,26 @@
      :joltage-diagram joltage-diagram}))
 
 (defn- button-press-combinations [buttons n]
-  (let [with-n-repeats (mapv (partial repeat n) buttons)]
+  (let [with-n-repeats (mapcat (partial repeat n) buttons)]
     (->> with-n-repeats
          count
          inc
          (range 1)
          (mapcat #(combo/combinations with-n-repeats %)))))
 
-(defn- press-buttons [initial-state buttons]
+(defn- press-buttons [initial-state buttons update-fn]
   (reduce (fn [new-state diagram-idx]
             (->> (nth new-state diagram-idx)
-                 not
+                 update-fn
                  (assoc new-state diagram-idx)))
           (vec initial-state)
           (flatten buttons)))
 
-(defn- min-btn-presses [desired-state-diagram buttons]
-  (let [initial-state (repeat (count desired-state-diagram) false)
-        button-combos (button-press-combinations buttons 1)]
+(defn- min-btn-presses [desired-state-diagram buttons n-combos update-fn initial-value]
+  (let [initial-state (repeat (count desired-state-diagram) initial-value)
+        button-combos (button-press-combinations buttons n-combos)]
     (->> button-combos
-         (filter #(= desired-state-diagram (press-buttons initial-state %)))
+         (filter #(= desired-state-diagram (press-buttons initial-state % update-fn)))
          (sort-by count)
          first
          count)))
@@ -63,7 +65,14 @@
   (let [instructions (map parse-instruction instruction-strs)]
     (->> instructions
          (map (fn [{:keys [light-diagram button-schematics]}]
-                (min-btn-presses light-diagram button-schematics)))
+                (min-btn-presses light-diagram button-schematics 1 not false)))
+         (reduce +))))
+
+(defn- fewest-presses-required-joltage-config [instruction-strs]
+  (let [instructions (map parse-instruction instruction-strs)]
+    (->> instructions
+         (map (fn [{:keys [joltage-diagram button-schematics]}]
+                (min-btn-presses joltage-diagram button-schematics (apply max joltage-diagram) inc 0)))
          (reduce +))))
 
 ;; ----------------------------------------------------------------------------
@@ -73,18 +82,18 @@
 
   (parse-instruction "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}")
 
-  (press-buttons [false true true false] [[1 3] [3]])
+  (press-buttons [false true true false] [[1 3] [3]] not)
 
-  (press-buttons [false false false false false] [[0 4] [0 1 2] [1 2 3 4]])
+  (press-buttons [false false false false false] [[0 4] [0 1 2] [1 2 3 4]] not)
 
   (button-press-combinations [[3] [1 3] [2] [2 3] [0 2] [0 1]] 1)
 
   ;; 2
-  (min-btn-presses [false true true false] [[3] [1 3] [2] [2 3] [0 2] [0 1]])
+  (min-btn-presses [false true true false] [[3] [1 3] [2] [2 3] [0 2] [0 1]] 1 not false)
   ;; 3
-  (min-btn-presses [false false false true false] [[0 2 3 4] [2 3] [0 4] [0 1 2] [1 2 3 4]])
+  (min-btn-presses [false false false true false] [[0 2 3 4] [2 3] [0 4] [0 1 2] [1 2 3 4]] 1 not false)
   ;; 2
-  (min-btn-presses [false true true true false true] [[0 1 2 3 4] [0 3 4] [0 1 2 4 5] [1 2]])
+  (min-btn-presses [false true true true false true] [[0 1 2 3 4] [0 3 4] [0 1 2 4 5] [1 2]] 1 not false)
 
   (def example-instructions ["[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"
                              "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}"
@@ -102,4 +111,10 @@
 ;; ----------------------------------------------------------------------------
 ;; PART TWO
 
-(comment)
+(comment
+
+  ;; 10
+  (min-btn-presses [3 5 4 7] [[3] [1 3] [2] [2 3] [0 2] [0 1]] 7 inc 0)
+
+  ;; 33
+  (fewest-presses-required-joltage-config example-instructions))
