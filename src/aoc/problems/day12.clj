34f84b7f720fb4aac9e3a71440679aeff3f1a47d
Jaxon Adams <jaxon.adams@loanpro.io>
2025-12-11 22:29:06 -0700

    day12: Parse shapes from input lines

--- src/aoc/problems/day12.clj
+++ src/aoc/problems/day12.clj
@@ -1,10 +1,21 @@
 (ns aoc.problems.day12
-  (:require [utils.files :refer [read-file-lines]]
+  (:require [aoc.utils.files :refer [read-file-lines]]
+            [aoc.utils.parsing :refer [split-at-value]]
             [clojure.string :as string]))
 
-(defn parse-input [input-lines]
-  (let []
-    {}))
+(defn- build-shape [shape-lines]
+  (letfn [(parse-line [line]
+            (map #(= % \#) line))]
+    (map parse-line shape-lines)))
+
+(defn- parse-input [input-lines]
+  (let [parts (split-at-value "" input-lines)
+        shapes (->> parts
+                    butlast
+                    (map rest)
+                    (map build-shape))
+        regions (last parts)]
+    {:shapes shapes :regions regions}))
 
 ;; ----------------------------------------------------------------------------
 ;; PART ONE
@@ -12,4 +23,6 @@
 (comment
 
   (def example-lines (read-file-lines "input/day12/example.txt"))
-  example-lines)
+
+  (let [parsed-input (parse-input example-lines)]
+    (-> parsed-input :shapes (nth 2))))
--- src/aoc/utils/parsing.clj
+++ src/aoc/utils/parsing.clj
@@ -1,5 +1,18 @@
 (ns aoc.utils.parsing)
 
+(defn- split-by [pred coll]
+  (lazy-seq
+   (when-let [s (seq coll)]
+     (let [[xs ys] (split-with pred s)]
+       (if (seq xs)
+         (cons xs (split-by pred ys))
+         (let [!pred (complement pred)
+               skip (take-while !pred s)
+               others (drop-while !pred s)
+               [xs ys] (split-with pred others)]
+           (cons (concat skip xs)
+                 (split-by pred ys))))))))
+
 (defn split-at-value [value coll]
-  (let [[left right] (split-with (complement #{value}) coll)]
-    [left (drop-while #{value} right)]))
+  (let [split (split-by (complement #{value}) coll)]
+    (into [(first split)] (map rest (rest split)))))
